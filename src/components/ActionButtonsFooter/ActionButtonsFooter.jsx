ab96bfed7a6d777c5f38599836b6c4f5dba323d6
Gabriel Peralta <gaperaltav@gmail.com>
2022-03-20 01:50:36 -0400

    Formating action footer

--- src/components/ActionButtonsFooter/ActionButtonsFooter.jsx
+++ src/components/ActionButtonsFooter/ActionButtonsFooter.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Button } from '@mui/material'
 import { Box } from '@mui/system'
 import SendIcon from '@mui/icons-material/Send'
 import SaveIcon from '@mui/icons-material/Save'
 
-function ActionButtonsFooter({ onSave, OnCancel }) {
+function ActionButtonsFooter({ onSave, onCancel }) {
   return (
     <Box xs={{
       padding: '10px',
     }}
     >
-      <Button variant="contained" endIcon={<SendIcon />}> Cancelar </Button>
-      <Button variant="contained" endIcon={<SaveIcon />}> Guardar </Button>
+      <Button
+        variant="contained"
+        endIcon={<SendIcon />}
+        onClick={onCancel}
+      >
+        Cancelar
+      </Button>
+      <Button
+        variant="contained"
+        endIcon={<SaveIcon />}
+        onClick={onSave}
+      >
+        Guardar
+      </Button>
     </Box>
   )
+}
+
+ActionButtonsFooter.propTypes = {
+  onSave: PropTypes.func,
+  onCancel: PropTypes.func,
+}
+
+ActionButtonsFooter.defaultProps = {
+  onSave: () => {},
+  onCancel: () => {},
 }
 
 export default ActionButtonsFooter
